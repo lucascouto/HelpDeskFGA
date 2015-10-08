e166c1d8a8efddd1a9e6ca80aedc6b7dec454034
tamar <tamara-bt@hotmail.com>
2015-10-08 16:50:13 -0300

    Criando Controladora de Chamado

---
+++ controller/ChamadoController.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once "/model/Chamado.php";
+
+class ChamadoController {
+
+    private $chamado;
+
+    public function __construct() {
+        
+    }
+
+    public function _criarChamado() {
+        $this->chamado = new Chamado;
+    }
+
+    public function _enviarChamado() {
+        
+    }
+
+    public function _atenderChamado() {
+        
+    }
+
+    public function _homologarChamado() {
+        
+    }
+
+}
