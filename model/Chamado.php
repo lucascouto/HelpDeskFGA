9674c4ed308439861ea6264babf02814ea66be39
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-10-08 15:54:49 -0300

    Criando construtores para Modelo de Chamado

--- model/Chamado.php
+++ model/Chamado.php
@@ -1,3 +1,39 @@
-<?php
+<!DOCTYPE html>
+<head>
+</head>
 
+<body>
+    <?php
+        class Chamado
+        /* 
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
 
+        private $descricao;
+        private $local;
+        private $tecnico;
+        private $patrimonio;
+        
+        public function __construct( ){
+            
+        }
+
+        public function __constructOverload( $local,$tecnico,$patrimonio,$descricao){
+	
+		this->$descricao = $descricao;
+                this->$local = $local;
+                this->$tecnico = $tecnico;
+                this->$patrimonio = $patrimonio;       
+	}
+        
+        public function __setDescricao ($descricao){
+		this->$descricao = $descricao;
+	}
+	
+	public function __getDescricao( ){
+		return this->$descricao;
+	}
+        
+?></body>
