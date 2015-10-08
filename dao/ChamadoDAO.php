d5b0c6d39bc923b8e1d85ca0908c7bb2395edc20
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-10-08 17:14:40 -0300

    Criando módulo de conexão com banco de Chamado

---
+++ dao/ChamadoDAO.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once "/model/Chamado.php";
+include_once "/dao/conecta.class.php";
+
+class ChamadoDAO {
+    
+    private $conexao;
+    
+    public function __construct( ){
+		$this->conexao = new Conexao( );
+	}
+        
+    public function inserirChamado(Chamado $chamado){
+		$sql = "INSERT INTO chamado (local,descricao,tecnico,patrimonio)
+                        VALUES ('{$chamado->__getLocal( )}',"
+                           . "'{$chamado->__getDescricao( )}',"
+                           . "'{$chamado->__getTecnico( )}',"
+                           . "'{$chamado->__getPatrimonio( )}' )";
+                           
+		$resultado = $this->conexao->Execute( $sql );
+		return $resultado;
+	}
+}
