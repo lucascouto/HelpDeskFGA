849d703f6317288ee10b922918e33c74c4ef77b5
tamar <tamara-bt@hotmail.com>
2015-10-08 19:02:45 -0300

    Criando método para receber dados do chamado

--- controller/ChamadoController.php
+++ controller/ChamadoController.php
@@ -1,17 +1,30 @@
 <?php
 
 include_once "/model/Chamado.php";
+include_once "/model/Local.php";
+include_once "/model/Patrimonio.php";
 
 class ChamadoController {
 
     private $chamado;
+    private $local;
+    private $patrimonio;
 
     public function __construct() {
-        
+        $this->chamado = new Chamado;
+        $this->local = new Local;
+        $this->patrimonio = new patrimonio;
     }
 
     public function criarChamado() {
-        $this->chamado = new Chamado;
+        $chamado->setDescricao = $_POST['chamadoDescricao'];
+        $local->setEdificio = $_POST['edificio'];
+        $local->setSala = $_POST['sala'];
+        $patrimonio->nome = $_POST['nomePatrimonio'];
+        $patrimonio->marca = $_POST['marcaPatrimonio'];
+        $patrimonio->codigo = $_POST['codPatrimonio'];
+        $patrimonio->tipo = $_POST['tipoPatrimonio'];
+        
     }
 
     public function enviarChamado() {
