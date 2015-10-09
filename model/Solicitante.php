7f4f744ceb15aae159a285782789e009701962e1
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-10-08 21:35:29 -0300

    Criando atributos e o construtor da classe Solicitante

--- model/Solicitante.php
+++ model/Solicitante.php
@@ -1,3 +1,19 @@
 <?php
+    class Solicitante{
 
+        private $nomeCompleto;
+        private $email;
+        private $matricula;
+        private $nomeUsuario;
+        private $vinculoUnb;
+        private $senha;
 
+        public function __construct($nomeCompleto, $email, $matricula, $nomeUsuario, $vinculoUnb, $senha){
+                $this->nomeCompleto = $nomeCompleto;
+                $this->email = $email;
+                $this->matricula = $matricula;
+                $this->nomeUsuario = $nomeUsuario;   
+                $this->vinculoUnb = $vinculoUnb;   
+                $this->senha = senha;
+        }
+}
