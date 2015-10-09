042f0a7f0aa4ef34e12bdc55c260955952da62af
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-10-08 21:36:22 -0300

    Criando Getters e Setters do Solicitante

--- model/Solicitante.php
+++ model/Solicitante.php
@@ -16,4 +16,50 @@
                 $this->vinculoUnb = $vinculoUnb;   
                 $this->senha = senha;
         }
+        
+        public function __getNomeCompleto( ){
+                return $this->nomeCompleto;
+        }
+        public function __setNomeCompleto ($nomeCompleto){
+                $this->nomeCompleto = $nomeCompleto;
+        }
+        
+        public function __getEmail( ){
+                return $this->email;
+        }
+        
+        public function __setEmail ($email){
+                $this->email = $email;
+        }
+
+        public function __getMatricula( ){
+                return $this->matricula;
+        }
+        public function __setMatricula ($matricula){
+                $this->matricula = $matricula;
+        }
+
+        public function __getNomeUsuario( ){
+                return $this->nomeUsuario;
+        }
+        
+        public function __setNomeUsuario ($nomeUsuario){
+                $this->nomeUsuario = $nomeUsuario;
+        }
+        
+        public function __getVinculoUnb( ){
+                return $this->vinculoUnb;
+        }
+        
+        public function __setVinculoUnb ($vinculoUnb){
+                $this->vinculoUnb = $vinculoUnb;
+        }
+        
+        public function __getSenha( ) {
+                return $this->senha;
+        }
+        
+        public function __setSenha($senha) {
+            $this->senha = $senha;
+        }
 }
